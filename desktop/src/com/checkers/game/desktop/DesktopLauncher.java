6954723a1c7a528c09866cb064610e86729e7060
yirman <german2496@hotmail.com>
2018-06-11 16:19:10 -0400

    initial commit

---
+++ desktop/src/com/checkers/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.checkers.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.checkers.game.Checkers;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Checkers(), config);
+	}
+}
